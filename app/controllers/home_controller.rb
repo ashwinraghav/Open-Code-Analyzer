63c05eedec962aecd101dcf0a2514cfe0bd50bde
Your Name <armohan@thoughtworks.com>
2010-10-27 04:26:39 +0530

    2 pointer, done. added upload form and a little bit of validation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    
+    redirect_to :controller => :code_submissions, :action => :new    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ ActionController::Routing::Routes.draw do |map|
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
 
-  map.root :controller => :home, :action => :index
+  map.root :controller => "home", :action => "index"
 
   map.resources :code_submissions
 end
