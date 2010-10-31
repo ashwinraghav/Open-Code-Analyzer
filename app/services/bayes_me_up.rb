efa823f4e159c3a2e1c4d51981c21870c97a3931
Mark Needham <m.h.needham@gmail.com>
2010-10-31 15:33:41 +0530

    simplifying nostradamus multiplication a little

--- app/services/bayes_me_up.rb
+++ app/services/bayes_me_up.rb
@@ -20,8 +20,8 @@ class BayesMeUp
     male = 1
     female = 1
     value.each_pair do |k, v|
-      male = male * probability_density_function(v, mean_for(:male, k), variance(:male, k))
-      female = female * probability_density_function(v, mean_for(:female, k), variance(:female, k))
+      male *= probability_density_function(v, mean_for(:male, k), variance(:male, k))
+      female *= probability_density_function(v, mean_for(:female, k), variance(:female, k))
     end
     male = male * 0.5
     female = female * 0.5
